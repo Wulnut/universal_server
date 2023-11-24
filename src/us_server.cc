2ecccf109c2b16c8a83abc0b63cd5d0d96995f66
LyZ <985951964@qq.com>
2023-11-24 18:45:33 +0800

    feat: 新增tcp网络连接层与业务层

--- src/us_server.cc
+++ src/us_server.cc
@@ -6,3 +6,45 @@
  * @description:
  ********************************************************************************/
 #include "include/us_server.h"
+#include "include/json.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace placeholders;
+using json = nlohmann::json;
+
+us_server::us_server(muduo::net::EventLoop* loop, const muduo::net::InetAddress& listenAddr,
+                     const std::string& nameArg)
+    : _server(loop, listenAddr, nameArg)
+    , _loop(loop)
+{
+    _server.setConnectionCallback(bind(&us_server::on_connection, this, _1));
+    _server.setMessageCallback(bind(&us_server::on_message, this, _1, _2, _3));
+    _server.setThreadNum(5);
+}
+
+void us_server::start()
+{
+    _server.start();
+}
+
+void us_server::on_connection(const muduo::net::TcpConnectionPtr& conn)
+{
+    if (!conn->connected()) {
+        // TODO clean up service logic
+        us_service::instance()->clientCloseException(conn);
+        conn->shutdown();
+    }
+}
+
+void us_server::on_message(const muduo::net::TcpConnectionPtr& conn, muduo::net::Buffer* buffer,
+                           muduo::Timestamp time)
+{
+    string buf = buffer->retrieveAllAsString();
+    json   msg = json::parse(buf);
+
+    LOG_DEBUG << msg.dump();
+    // TODO handle service logic
+    auto msgHandler = us_service::instance()->getHandler(msg["msgId"].get<int>());
+    msgHandler(conn, msg, time);
+}
--- src/us_service.cc
+++ src/us_service.cc
@@ -5,4 +5,39 @@
  * @version: 1.0
  * @description:
  ********************************************************************************/
-#include "us_service.h"
+#include "include/us_service.h"
+
+us_service::us_service()
+{
+    LOG_INFO << "Hello World us_service";
+}
+
+us_service* us_service::instance()
+{
+    static us_service service;
+    return &service;
+}
+
+MsgHandler us_service::getHandler(int msgId)
+{
+    auto it = _msgHandlerMap.find(msgId);
+
+    if (it == _msgHandlerMap.end()) {
+        return [=](const muduo::net::TcpConnectionPtr& conn, json& msg, muduo::Timestamp) {
+            LOG_ERROR << "msId: " << msgId << " can not find handler";
+        };
+    }
+    else {
+        return _msgHandlerMap[msgId];
+    }
+}
+
+void us_service::clientCloseException(const muduo::net::TcpConnectionPtr& conn)
+{
+    // TODO handler exception
+}
+
+void us_service::rest()
+{
+    // TODO reset service server
+}
