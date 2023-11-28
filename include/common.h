385119a3a1fa4def75e055711b20d7bf5bc0f10a
LyZ <985951964@qq.com>
2023-11-28 18:50:20 +0800

    feat: 新增common头文件用于规范返回类型

---
+++ include/common.h
@@ -0,0 +1,27 @@
+/********************************************************************************
+ * @author: Wulnut
+ * @email: carepdime@gmail.com
+ * @date: 2023/11/28 17:27
+ * @version: 1.0
+ * @description:
+ ********************************************************************************/
+#ifndef UNIVERSAL_SERVER_COMMON_H
+#define UNIVERSAL_SERVER_COMMON_H
+
+typedef enum us_code
+{
+    CODE_AUTH = 1000,
+    CODE_AUTH_ACK,
+    CODE_CONNECT,
+    CODE_CONNECT_ACK,
+    CODE_PHOTO,
+    CODE_PHOTO_ACK,
+} us_code_t;
+
+typedef enum us_result
+{
+    RESULT_OK          = 0,
+    RESULT_ID_NO_MATCH = -1000,
+} us_result_t;
+
+#endif   // UNIVERSAL_SERVER_COMMON_H
