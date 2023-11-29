0d69cb9ea575f6621ea93f0ccfca557a1187428a
LyZ <985951964@qq.com>
2023-11-29 17:29:35 +0800

    style: 修改入口函数心跳超时时间

--- main.cc
+++ main.cc
@@ -19,8 +19,9 @@ void reset_handler(int)
 
 int main(int argc, char** argv)
 {
-    char*    ip   = nullptr;
-    uint16_t port = 0;
+    char*    ip     = nullptr;
+    char*    endptr = nullptr;
+    uint16_t port   = 0;
 
     if (argc < 3) {
         printf("Command invalid! example: ./us_server 127.0.0.1 6000\n");
@@ -28,14 +29,24 @@ int main(int argc, char** argv)
     }
 
     ip   = argv[1];
-    port = strtol(argv[2], nullptr, 4);
+    port = strtol(argv[2], &endptr, 10);
+
+    if (ip == nullptr) {
+        printf("ip error\n");
+        exit(-1);
+    }
+
+    if (*endptr != '\0') {
+        printf("Conversion error, non-convertible part: %s\n", endptr);
+        exit(-1);
+    }
 
     signal(SIGINT, reset_handler);
     Logger::setLogLevel(Logger::DEBUG);
 
     EventLoop   loop;
     InetAddress addr(ip, port);
-    us_server   server(&loop, addr, "us_server");
+    us_server   server(&loop, addr, 10, "us_server");
 
     server.start();
     loop.loop();
