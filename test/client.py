f59e72724b98a55dcaa2c2ed55f13c019dd6fc7e
LyZ <985951964@qq.com>
2023-12-06 16:52:43 +0800

    feat: 新增多线程连接进行压力测试

--- test/client.py
+++ test/client.py
@@ -3,9 +3,9 @@
 import socket
 import json
 import base64
-import string
 import uuid
 import time
+import threading
 
 host = "127.0.0.1"
 port = 6000
@@ -17,11 +17,9 @@ def generator_uuid():
 
     return res
 
-
-if __name__ == '__main__':
+def tcp_conn(i):
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     client.connect((host, port))
 
     sequence = generator_uuid()
@@ -38,11 +36,29 @@ if __name__ == '__main__':
 
     json_object = json.dumps(send, indent = 4)
 
-    client.send(json_object.encode("utf-8"))
-    
-    msg = client.recv(1024)
-
-    print("receive: ", msg)
+    for i in range(10000):
+        client.send(json_object.encode("utf-8"))
+        msg = client.recv(1024)
+        print("receive: ", msg.decode("utf-8"))
 
     client.close()
+
+def worker(thread_id):
+    print(f"Thread {thread_id} is running")
+
+
+if __name__ == '__main__':
+
+    num_threads = 10
+    threads = []
+
+    for i in range(num_threads):
+        thread = threading.Thread(target = tcp_conn, args = (i, ))
+        threads.append(thread)
+        thread.start()
+    
+    for thread in threads:
+        thread.join()
+
+
     
