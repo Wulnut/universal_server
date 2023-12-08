9d848870cea68f52bf5acbb1ae56e2bb455d8fd8
LyZ <985951964@qq.com>
2023-12-08 17:13:45 +0800

    fix: 重写base64编码

--- include/common.h
+++ include/common.h
@@ -15,8 +15,8 @@
 #include <string>
 #include <uuid/uuid.h>
 
-std::string base64_encode(const std::string& buffer);
-std::string base64_decode(const std::string& encoded);
+std::string base64_encode(char const* bytes_to_encode, int in_len);
+std::string base64_decode(std::string& encoded_string);
 std::string uuid_generator();
 
 typedef enum us_code
--- src/common.cc
+++ src/common.cc
@@ -7,49 +7,90 @@
  ********************************************************************************/
 #include "include/common.h"
 
-std::string base64_encode(const std::string& buffer)
+
+static const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                        "abcdefghijklmnopqrstuvwxyz"
+                                        "0123456789+/";
+
+static inline bool is_base64(unsigned char c)
 {
-    // 创建一个内存 BIO，用于结果的存储
-    BIO* bio      = BIO_new(BIO_s_mem());
-    BIO* b64      = BIO_new(BIO_f_base64());
-    bio           = BIO_push(b64, bio);
-    char* encoded = nullptr;
-    long  len     = 0;
-
-    // 确保不自动换行
-    BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
-
-    // 将数据写入 BIO
-    BIO_write(bio, buffer.c_str(), (int)buffer.length());
-    BIO_flush(bio);
-
-    // 从内存 BIO 读取编码后的数据
-    len = BIO_get_mem_data(bio, &encoded);
-    std::string result(encoded, len);
-
-    // 释放 BIO
-    BIO_free_all(bio);
-
-    return result;
+    return (isalnum(c) || (c == '+') || (c == '/'));
 }
 
-std::string base64_decode(const std::string& encoded)
+std::string base64_encode(char const* bytes_to_encode, int in_len)
 {
-    BIO*          bio            = nullptr;
-    BIO*          b64            = nullptr;
-    int           decoded_length = 0;
-    unsigned char buffer[encoded.length()];
-    memset(buffer, 0, sizeof(buffer));
+    std::string   ret;
+    int           i = 0;
+    int           j = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
 
-    bio = BIO_new_mem_buf(encoded.c_str(), -1);
-    b64 = BIO_new(BIO_f_base64());
-    bio = BIO_push(b64, bio);
+    while (in_len--) {
+        char_array_3[i++] = *(bytes_to_encode++);
+        if (i == 3) {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
 
-    decoded_length = BIO_read(bio, buffer, (int)encoded.length());
-    std::string decoded((char*)buffer, decoded_length);
+            for (i = 0; (i < 4); i++) ret += base64_chars[char_array_4[i]];
+            i = 0;
+        }
+    }
 
-    BIO_free_all(bio);
-    return decoded;
+    if (i) {
+        for (j = i; j < 3; j++) char_array_3[j] = '\0';
+
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[3] = char_array_3[2] & 0x3f;
+
+        for (j = 0; (j < i + 1); j++) ret += base64_chars[char_array_4[j]];
+
+        while ((i++ < 3)) ret += '=';
+    }
+
+    return ret;
+}
+
+std::string base64_decode(std::string& encoded_string)
+{
+    int           in_len = encoded_string.size();
+    int           i      = 0;
+    int           j      = 0;
+    int           in_    = 0;
+    unsigned char char_array_4[4], char_array_3[3];
+    std::string   ret;
+
+    while (in_len-- && (encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
+        char_array_4[i++] = encoded_string[in_];
+        in_++;
+        if (i == 4) {
+            for (i = 0; i < 4; i++) char_array_4[i] = base64_chars.find(char_array_4[i]);
+
+            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+            for (i = 0; (i < 3); i++) ret += char_array_3[i];
+            i = 0;
+        }
+    }
+
+    if (i) {
+        for (j = i; j < 4; j++) char_array_4[j] = 0;
+
+        for (j = 0; j < 4; j++) char_array_4[j] = base64_chars.find(char_array_4[j]);
+
+        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+        char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+        for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+    }
+
+    return ret;
 }
 
 std::string guid_to_string(const GUID& guid)
